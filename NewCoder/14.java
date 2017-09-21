2de9bd87c5f9c5ab89ad22ab0b1136c6f6b439b0
whing123 <whpiggys@gmail.com>
2017-09-21 18:53:42 +0800

    人口普查

---
+++ NewCoder/14.java
@@ -0,0 +1,113 @@
+
+import java.util.*;
+
+public class Hello{ 
+	
+	public static void main(String[] args) {
+		final String today = "2014/9/6";
+		final Age todayPerson = Age.newPerson(today);
+		final String twoHundred = "1814/9/6";
+		final Age oldPerson = Age.newPerson(twoHundred);
+
+		int count = 0;
+		
+		Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        
+        // store oldest and youngest person
+        Age older = Age.newPerson(today);
+        Age newer = Age.newPerson("1814/9/6");
+        
+        int i = 0;
+        String name;
+        String birthday;
+        while(i < N){
+        	name = in.next();
+        	birthday = in.next();
+        	Age tmp = Age.newPerson(birthday);
+        	
+        	i++;
+        	
+        	// > 200
+        	/*if(tmp.isTooOld(todayPerson)){
+        		continue;
+        	}*/
+        	if(tmp.compareTo(oldPerson) > 0){
+        		continue;
+        	}
+        	
+        	// not born yet
+        	if(tmp.compareTo(todayPerson) < 0){
+        		continue;
+        	}
+        	
+        	// valid
+        	count++;
+        	
+        	// younger
+        	if(tmp.compareTo(newer) <= 0){
+        		newer = tmp;
+        		newer.name = name;
+        	}
+        	
+        	// older
+        	if(tmp.compareTo(older) >= 0){
+        		older = tmp;
+        		older.name = name;
+        	}
+        }
+        
+        // print
+		System.out.println(count + " " + older.name + " " + newer.name);
+
+	}
+}
+
+class Age implements Comparable<Age>{
+    int year;
+    int month;
+    int day;
+    String name;
+    
+    public Age(int y, int m, int d){
+    	year = y;
+    	month = m;
+    	day = d;
+    	name = null;
+    }
+    
+    // create a person
+    public static Age newPerson(String time){
+    	String [] ymd = time.split("/");
+    	return new Age(Integer.parseInt(ymd[0]),Integer.parseInt(ymd[1]),Integer.parseInt(ymd[2]));
+    }
+    
+    // compare if older or younger
+    public int compareTo(Age tmp){
+    	int res = -1;
+    	if(year < tmp.year){
+    		res = 1;
+    	}else if(year == tmp.year && month < tmp.month){
+    		res = 1;
+    	}else if(year == tmp.year && month == tmp.month && day < tmp.day){
+    		res = 1;
+    	}else if(year == tmp.year && month == tmp.month && day == tmp.day){ // ==
+    		res = 0;
+    	}
+    	return res;
+    }
+    
+    // check if exceeding 200
+    boolean isTooOld(Age tmp){
+    	boolean res = false;
+    	if(tmp.year - year > 200){
+    		res = true;
+    	}else if(tmp.year - year == 200 && tmp.month > month){
+    		res = true;
+    	}else if(tmp.year - year == 200 && tmp.month == month && tmp.day > day){
+    		res = true;
+    	}
+    	
+    	return res;
+    }    
+}
